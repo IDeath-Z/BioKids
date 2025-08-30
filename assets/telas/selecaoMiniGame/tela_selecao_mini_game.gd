5e53d6c06e6552bb1b228935e4c470953018c78b
DeathZ <jose.joz46@hotmail.com>
2025-08-30 14:48:19 -0300

    style: modifica a aparência dos menus

--- assets/telas/selecaoMiniGame/tela_selecao_mini_game.gd
+++ assets/telas/selecaoMiniGame/tela_selecao_mini_game.gd
@@ -2,3 +2,6 @@ extends Control
 
 func _on_mini_game_raio_x_pressed() -> void:
 	get_tree().change_scene_to_file("res://assets/telas/raioX/raio_x_menu.tscn")
+
+func _on_botao_voltar_pressed() -> void:
+	get_tree().change_scene_to_file("res://assets/telas/inicio/tela_inicial.tscn")
