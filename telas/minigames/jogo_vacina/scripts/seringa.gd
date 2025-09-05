52e7b08b9c472a5a4345f60b9e9560dcaca1bdc2
Yosaf <yosaf.marques55@gmail.com>
2025-09-04 22:13:11 -0300

    Refatorei código

--- telas/minigames/jogo_vacina/scripts/seringa.gd
+++ telas/minigames/jogo_vacina/scripts/seringa.gd
@@ -1,24 +1,23 @@
 extends Area2D
 
-var segurando = false
-var toque = Vector2.ZERO
 @onready var braco = $braco
 
-func _ready():
-	braco.visible = false	
+var segurando = false
+var toque = Vector2.ZERO
 
-func _input_event(viewport, event, shape_idx):
-	if event is InputEventScreenTouch or event is InputEventMouseButton:
+func _ready():
+	braco.visible = false
+
+func _input(event):
+	if event is InputEventMouseButton or event is InputEventScreenTouch:
 		if event.pressed:
 			segurando = true
 			toque = global_position - event.position
-			braco.visible = true 
+			braco.visible = true
 		else:
 			segurando = false
 			braco.visible = false
-	if event is InputEventScreenDrag:
-		if segurando:
-			global_position = event.position + toque
-	if event is InputEventMouseMotion:
+
+	elif event is InputEventMouseMotion or event is InputEventScreenDrag:
 		if segurando:
 			global_position = event.position + toque
