35a2ec95c86f7d4e8b8db7e9afbb70272087ff65
DeathZ <jose.joz46@hotmail.com>
2025-09-12 16:33:07 -0300

    feat: implementa scanner na tela de raio-x e transição entre telas

--- telas/minigames/raioX/raio_x_camera.gd
+++ telas/minigames/raioX/raio_x_camera.gd
@@ -1,14 +1,20 @@
 extends Control
 
-@onready var camera_display = $CameraDisplay
-@onready var camera_container = $CameraDisplay/SubViewport
 @onready var camera_view = $CameraDisplay/SubViewport/CameraView
-@onready var timer = $ContaineBotaoScanner/Timer
-@onready var scanner_button = $ContaineBotaoScanner/BotaoCliqueAqui
+@onready var cor_fundo = $Fundo
+@onready var animacao_fundo = $Fundo/AnimacaoFundo
+@onready var animacar_texto = $AspectRatioContainerDicas/PopUpDicas/Label/AnimacaoTexto
+@onready var touch_button = $TouchScreen
+@onready var animacao_scanner = $Scanner/AnimacaoScanner
+@onready var mao_click = $MaoClick
+@onready var timer_mao = $TimerMao
 
 var android_camera: AndroidCamera
+var toque_na_tela = false
 
 func _ready() -> void:
+	timer_mao.start()
+	mao_click.hide()
 	
 	if OS.get_name() == "Android":
 		android_camera = AndroidCamera.new()
@@ -31,10 +37,28 @@ func _on_stop_capturing_pressed() -> void:
 	if android_camera:
 		android_camera.stop_camera()
 
-func _on_botao_clique_aqui_pressed() -> void:
-	timer.start()
+func _on_touch_screen_pressed() -> void:
+	toque_na_tela = true
+	animacao_fundo.play("fade")
+	animacao_scanner.play("mover")
+	animacar_texto.play("remover_linha")
+	touch_button.disabled = true
+	mao_click.hide()
+	mao_click.stop()
 	
-func _on_timer_timeout() -> void:
-	if OS.get_name() == "Android":
-		_on_stop_capturing_pressed()
+func _on_timer_mao_timeout() -> void:
+	if !toque_na_tela:
+		mao_click.show()
+		mao_click.play("click")
+
+func _on_animacao_scanner_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "mover":
+		animacar_texto.play("fade")
+		animacao_fundo.play("cobrir")
+		
+func _on_animacao_fundo_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "cobrir":
+		if OS.get_name() == "Android":
+			_on_stop_capturing_pressed()
+			
 		get_tree().change_scene_to_file("res://telas/minigames/raioX/raio_x_imagem.tscn")
--- telas/minigames/raioX/raio_x_imagem.gd
+++ telas/minigames/raioX/raio_x_imagem.gd
@@ -1,8 +1,31 @@
 extends Control
 
+@onready var area_mao = $AreaMao
+@onready var animacao_mao = $AreaMao/AnimacaoMao
+@onready var texto = $AspectRatioContainerDicas/PopUpDicas/Label
+@onready var animacao_texto = $AspectRatioContainerDicas/PopUpDicas/Label/AnimacaoTexto
+@onready var botoes = $HBoxContainer
+@onready var animacao_botoes = $HBoxContainer/AnimacaoBotoes
+
+func _ready() -> void:
+	area_mao.visible = false
+	texto.visible = false
+	botoes.visible = false
+	animacao_mao.play("fade")
+	animacao_texto.play("fade")
+	animacao_botoes.play("fade")
+
 func _on_botao_sim_pressed() -> void:
 	pass # Fazer a logica pra ir pra tela de raio-x  de novo
 	
 func _on_botao_nao_pressed() -> void:
 	# Colocar a tela do biofato
 	get_tree().change_scene_to_file("res://telas/minigames/raioX/raio_x_menu.tscn")
+
+
+func _on_animacao_mao_animation_started(anim_name: StringName) -> void:
+	if anim_name == "fade":
+		await get_tree().create_timer(0.1).timeout
+		area_mao.visible = true
+		texto.visible = true
+		botoes.visible = true
