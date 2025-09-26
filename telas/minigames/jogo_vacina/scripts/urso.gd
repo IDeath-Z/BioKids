dd8869fc0a5419e8f6d71a31f1410378911af674
Yosaf <yosaf.marques55@gmail.com>
2025-09-25 21:47:04 -0300

    movimentacao local vacina

--- telas/minigames/jogo_vacina/scripts/urso.gd
+++ telas/minigames/jogo_vacina/scripts/urso.gd
@@ -8,23 +8,34 @@ signal conquista_feita
 
 func _ready():
 	# Posição inicial fora da tela
-	macoste.position = Vector2(990, 500)
-	banqueta.position = Vector2(990, 600)
-	colisao.position = Vector2(990, 500)
-
-	# Tween para macoste
-	var tween1 = get_tree().create_tween()
-	tween1.tween_property(macoste, "position", Vector2(500, 550), 1.5)
-
-	# Tween para banqueta
-	var tween2 = get_tree().create_tween()
-	tween2.tween_property(banqueta, "position", Vector2(510, 800), 1.5)
-	
-	var tween3 = get_tree().create_tween()
-	tween2.tween_property(colisao, "position", Vector2(455, 620), 1.5)
+	macoste.position = Vector2(371.2, -270.6)
+	colisao.visible = false
+	colisao.position = Vector2(306, 618)
 	
 	connect("area_entered", Callable(self, "_on_area_entered"))
+	
+	# Tween para macoste
+	var tween1 = get_tree().create_tween()
+	tween1.tween_property(macoste, "position", Vector2(345, 565), 1.8)
+	
+	await tween1.finished
+	movimentar_local_vacina()
+	
+func movimentar_local_vacina():
 
+	colisao.visible = true
+	
+	var locais = [
+		Vector2(400, 618),
+		Vector2(300, 650),
+		Vector2(410, 660),
+	]
+
+	for pos in locais:
+		var tween_move = get_tree().create_tween()
+		tween_move.tween_property(colisao, "position", pos, 1.5)
+		await tween_move.finished
+	
 func _on_area_entered(area):	
 	if area.name == "seringa":
 		emit_signal("conquista_feita")
