8ff8974a40404e20552cf278ecd3bc3d0e322c27
VitorBorgess1 <borgesvitor49556@gmail.com>
2025-09-29 18:13:33 -0300

    bio_goods - style tela inicial e feat tela selecionar_quadro

---
+++ telas/minigames/bio_goods/scripts/home.gd
@@ -0,0 +1,13 @@
+extends Control
+
+func _ready() -> void:
+	pass # Pode deixar vazio ou inicializar algo aqui.
+
+func _process(delta: float) -> void:
+	pass # Só use se precisar rodar algo a cada frame.
+
+func _on_iniciar_pressed() -> void:
+	get_tree().change_scene_to_file("res://telas/minigames/bio_goods/scene/selecionar_quadro.tscn")
+
+func _on_sair_pressed() -> void:
+	get_tree().change_scene_to_file("res://telas/interface/selecaoMiniGame/tela_selecao_mini_game_transicao.tscn")
