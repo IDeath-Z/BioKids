68de9fe1fec1f635aa46b2543098736b1fa44b1a
ZeSapeka <sir1jos1.contato@gmail.com>
2025-09-30 10:58:54 -0300

    Bio-Escudo - Melhorias na interface

--- telas/minigames/bio_escudo/Scenes/finished_button.gd
+++ telas/minigames/bio_escudo/Scenes/finished_button.gd
@@ -1,4 +1,4 @@
 extends TextureButton
 
 func _on_pressed() -> void:
-	get_tree().change_scene_to_file("res://telas/interface/bioFato/bio_fato.tscn")
+	get_tree().change_scene_to_file("res://telas/minigames/bio_escudo/Scenes/bioFato/bio_fato_EPIs.tscn")
