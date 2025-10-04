32de14c4afce20033527d3818310e9178a636b85
Yosaf <yosaf.marques55@gmail.com>
2025-10-04 19:10:40 -0300

    Alterando urso

--- telas/minigames/jogo_vacina/scripts/main.gd
+++ telas/minigames/jogo_vacina/scripts/main.gd
@@ -13,6 +13,8 @@ func _ready():
 	# aqui você escolhe qual cena vem primeiro
 	change_scene("seringa")
 	
+	ajustar_escala_telas()
+	
 func change_scene(scene_name: String):
 	if current_scene:
 		current_scene.queue_free()
@@ -29,3 +31,19 @@ func change_scene(scene_name: String):
 		
 func _on_conquista_feita():
 	get_tree().change_scene_to_file("res://telas/minigames/jogo_vacina/cenas/Conquista_01.tscn")
+
+
+# resolução base (a que você usou pra desenvolver o layout)
+const BASE_SIZE := Vector2(720, 1080)
+
+func ajustar_escala_telas():
+	# tamanho real da tela do dispositivo
+	var screen_size = get_viewport_rect().size
+	# calcula o fator de escala proporcional
+	var scale_factor = screen_size.y / BASE_SIZE.y 
+	# percorre todas as telas instanciadas dentro da Main
+	for child in get_children():
+		# aplica a escala apenas nos nós do tipo Control (telas)
+		if child is Control:
+			child.scale = Vector2(scale_factor, scale_factor)
+	
