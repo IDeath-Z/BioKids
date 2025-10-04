02e7196e87e1b66df13fd05680e8d7d5c02ee06d
DeathZ <jose.joz46@hotmail.com>
2025-10-04 14:12:01 -0300

    style: altera o como jogar da tela de biovacina

--- telas/minigames/jogo_vacina/scripts/home.gd
+++ telas/minigames/jogo_vacina/scripts/home.gd
@@ -4,52 +4,35 @@ extends Control
 @onready var botao_iniciar = $MarginContainerPrincipal2/GridBotoes/BotaoIniciar
 @onready var botao_como_jogar = $MarginContainerPrincipal2/GridBotoes/BotaoComoJogar
 @onready var botao_voltar = $MarginContainerPrincipal2/GridBotoes/BotaoVoltar
-@onready var tela_02 = $Tela_02
-@onready var balao = $Tela_02/TexturaBalao
-@onready var texto_balao = $Tela_02/TextoBalao
-@onready var urso = $Tela_02/Sprite2D
 @onready var tela_01 = $Tela_01
 @onready var box_botoes = $MarginContainerPrincipal2
-@onready var botao_iniciar2 = $Tela_02/BotaoIniciar2
+@onready var textura_balao = $TexturaBalao
+@onready var animacao_balao = $TexturaBalao/AnimacaoBalao
+@onready var textura_urso = $TexturaUrso
+@onready var animacao_urso = $TexturaUrso/AnimacaoUrso
+
+func _ready() -> void:
+	textura_balao.visible = false
+	textura_urso.visible = false
 
 func _on_botao_voltar_pressed() -> void:
 	get_tree().change_scene_to_file("res://telas/interface/selecaoMiniGame/tela_selecao_mini_game.tscn")
 	
 func _on_botao_como_jogar_pressed() -> void:
-	chamar_home()
+	textura_balao.visible = true
+	textura_urso.visible = true
+	animacao_urso.play_backwards("sair_tela")
+	botao_como_jogar.disabled = true
 
 func _on_botao_jogar_pressed() -> void:
 	get_tree().change_scene_to_file("res://telas/minigames/jogo_vacina/cenas/main.tscn")
-
-func chamar_home():
-	var largura_tela := 720  # largura da sua tela
-
-	# Inicializar posições
-	tela_02.position = Vector2(-largura_tela, 0)  # fora da tela à esquerda
-	tela_01.position = Vector2(0, 0)             # tela 01 visível
-	tela_02.visible = true
-	tela_02.z_index = tela_01.z_index + 1        # garante que tela_02 fique acima de tela_01
-	box_botoes.visible = false
-
-	# Inicializar posições dos elementos internos da tela_02
-	balao.position = Vector2(11, 369)
-	texto_balao.position = Vector2(226, 417)
-	urso.position = Vector2(156, 914)
-
-	# Criar um único tween
-	var tween = get_tree().create_tween()
-
-	# Tela 2 entra primeiro (começa imediatamente)
-	tween.tween_property(tela_02, "position", Vector2(0,0), 1.0).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
-
-	# Tela 1 sai para a direita, mas com delay de 0.3s
-	tween.tween_property(tela_01, "position", Vector2(largura_tela, 0), 1.0).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT).set_delay(0.3)
-
-	# Callback para esconder tela_01 no final
-	tween.finished.connect(Callable(self, "_on_tela01_anim_finished"))
 
 func _on_tela01_anim_finished():
 	tela_01.visible = false
 
 func _on_botao_iniciar2_pressed() -> void:
 	get_tree().change_scene_to_file("res://telas/minigames/jogo_vacina/cenas/main.tscn")
+
+func _on_animacao_urso_animation_finished(anim_name: StringName) -> void:
+	animacao_balao.play("fade")
+	
