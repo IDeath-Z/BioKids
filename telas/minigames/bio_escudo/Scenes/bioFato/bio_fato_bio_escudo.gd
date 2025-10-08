ebc3073ce76727b28e391984e6fdfa3aececd64e
DeathZ <jose.joz46@hotmail.com>
2025-10-08 10:10:51 -0300

    fix: pequenos ajustes na responsividade das telas

--- telas/minigames/bio_escudo/Scenes/bioFato/bio_fato_bio_escudo.gd
+++ telas/minigames/bio_escudo/Scenes/bioFato/bio_fato_bio_escudo.gd
@@ -1,4 +1,7 @@
 extends Control
 
-func _on_botao_continuar_pressed() -> void:
-	get_tree().change_scene_to_file("res://telas/interface/selecaoMiniGame/tela_selecao_mini_game.tscn")
+func _on_bio_fato_botao_audio_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_bio_fato_botao_continuar_pressed() -> void:
+	get_tree().change_scene_to_file("res://telas/minigames/bio_escudo/Scenes/main.tscn")
