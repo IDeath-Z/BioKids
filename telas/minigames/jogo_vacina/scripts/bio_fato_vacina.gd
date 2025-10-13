b574e380944fc91c9b29e480f280921d157149da
Yosaf <yosaf.marques55@gmail.com>
2025-10-13 13:37:03 -0300

    Ajustando audio no bio fato do jogo da vacina

--- telas/minigames/jogo_vacina/scripts/bio_fato_vacina.gd
+++ telas/minigames/jogo_vacina/scripts/bio_fato_vacina.gd
@@ -1,16 +1,24 @@
 extends Control
 
-@onready var fala_urso = $AudioStreamPlayer
+@onready var player_de_audio = $AudioStreamPlayer
 
+func _ready() -> void:
+	add_child(player_de_audio)
+	player_de_audio.stream = preload("res://telas/minigames/jogo_vacina/sounds/bio_fato_audio.wav")
+	
+	player_de_audio.connect("finished", Callable(self, "_on_bio_fato_audio_finished"))
+	
 func _on_botao_continuar_pressed() -> void:
 	get_tree().change_scene_to_file("res://telas/interface/selecaoMiniGame/tela_selecao_mini_game.tscn")
 
 func _on_bio_fato_botao_continuar_pressed() -> void:
 	get_tree().change_scene_to_file("res://telas/interface/selecaoMiniGame/tela_selecao_mini_game.tscn")
 
-func _on_botao_audio_pressed() -> void:
-	fala_urso.play()
-
 
 func _on_bio_fato_botao_audio_pressed() -> void:
-	fala_urso.play()
+	MusicPlayer.parar_para_evento_especial()
+	player_de_audio.play()
+	
+func _on_bio_fato_audio_finished() -> void:
+	MusicPlayer.restaurar_gerenciamento_automatico()
+	
