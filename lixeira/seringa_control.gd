c94a0f786329913c1b23bc869083c880ff2736b0
Jose Antonio <162039904+IDeath-Z@users.noreply.github.com>
2025-10-17 14:55:57 -0300

    Fazendo a faxina

--- lixeira/seringa_control.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-@onready var botao_voltar = $AspectRatioContainer2/BotaoVoltar
-
-signal botao_voltar_pressed
-
-func _on_botao_voltar_pressed() -> void:
-	botao_voltar_pressed.emit()
